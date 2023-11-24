bb6578b52f6b52d9b5f9dd13725abe284413bc67
DomantasGarsvas <domantas.garsvas@tutotoons.com>
2023-11-24 13:08:38 +0200

    Added different spawning for host and client, put managers into prefabs

---
+++ Assets/Scripts/Networking/NetworkPlayerSpawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class NetworkPlayerSpawner : NetworkBehaviour
+{
+    [SerializeField] private GameObject explorerPrefab;
+    [SerializeField] private GameObject navigatorPrefab;
+
+    [SerializeField] private Transform explorerSpawnPoint;
+    [SerializeField] private Transform navigatorSpawnPoint;
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+            SpawnPlayerServerRpc(NetworkManager.Singleton.LocalClientId, 0);
+        }
+        else
+        {
+            SpawnPlayerServerRpc(NetworkManager.Singleton.LocalClientId, 1);
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void SpawnPlayerServerRpc(ulong _clientId, int _prefabId)
+    {
+        Vector3 _spawnPos = _prefabId == 0 ? explorerSpawnPoint.position : navigatorSpawnPoint.position;
+        GameObject _playerObj = _prefabId == 0 ? Instantiate(explorerPrefab, _spawnPos, Quaternion.identity) : Instantiate(navigatorPrefab, _spawnPos, Quaternion.identity);
+
+        _playerObj.SetActive(true);
+        _playerObj.GetComponent<NetworkObject>().SpawnAsPlayerObject(_clientId, true);
+    }
+}
